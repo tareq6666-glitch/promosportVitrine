12308dd10fb8c017ea4cf975366fefc96a94a149
tareq6666-glitch <tarekmouaddeb01@hotmail.com>
2025-09-02 16:59:56 +0100

    feat: sort actualites by order

--- src/app/actualites/actualites.service.ts
+++ src/app/actualites/actualites.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export interface Actualite {
   title: string;
   url: string;
+  order: number;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -12,6 +14,8 @@ export class ActualitesService {
   constructor(private http: HttpClient) {}
 
   getActualites(): Observable<Actualite[]> {
-    return this.http.get<Actualite[]>('assets/actualites.json');
+    return this.http
+      .get<Actualite[]>('assets/actualites.json')
+      .pipe(map(list => list.sort((a, b) => a.order - b.order)));
   }
 }
